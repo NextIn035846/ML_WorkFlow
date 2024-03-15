01cb32f2a0492dcd6a18e38464fc25bbd6848fce
Thomas <thomaspatole19@gmail.com>
2024-03-15 12:07:08 +0530

    Template of files

---
+++ Template.py
@@ -0,0 +1,51 @@
+import os 
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
+list_of_files =[
+    ".github/workflows/.gitkeep",
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evaluation.py",
+    "src/pipline/__init__.py",
+    "src/pipline/traning_pipeline.py",
+    "src/pipline/prediction_pipeline.py",
+    "src/utils/utils.py",
+    "src/utils/__init__.py",
+    "src/logger/logging.py",
+    "src/exception/exception.py",
+    "test/unit/__init__.py",
+    "test/integration/__init__.py",
+    "init_setup.sh",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "experiment/experiments.ipynb"
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir,filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for file {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exists")
